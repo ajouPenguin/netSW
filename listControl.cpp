ecbd4c1f51b52a9d526ebcfe95f95c0483b7fdda
root <root@ip-172-31-6-19.ap-northeast-2.compute.internal>
2017-05-28 12:22:14 +0000

    insert and delete code

--- listControl.cpp
+++ listControl.cpp
@@ -3,13 +3,13 @@
 #include <queue>
 #include <pthread.h>
 #include <mysql.h>
+#include <memory.h>
 
 using namespace std;
 
 int findURL(MYSQL *mysql, char* list, char* string)
 {
   char query[255];
-  MYSQL_RES *sql_result;
 
   sprintf(query, "select * from %s where %s", list, string);
   printf("%s\n", query);
@@ -19,9 +19,58 @@ int findURL(MYSQL *mysql, char* list, char* string)
      return 0;
   }
 
-  sql_result = mysql_store_result(mysql);
-
   return 1;
+}
+
+int insertUrl(MYSQL *mysql, char* list, char* serverIP)
+{
+    char query[255];
+
+    sprintf(query, "insert into %s values('%s')",list, serverIP);
+    printf("%s\n", query);
+    if(mysql_query(mysql, query))
+    {
+        printf("%s\n", mysql_error(mysql));
+        return 0;
+    }
+
+    return 1;
+}
+
+int deleteUrl(MYSQL *mysql, char* list, char* serverIP)
+{
+    char query[255];
+    MYSQL_RES *sql_result;
+    MYSQL_ROW row;
+
+    sprintf(query, "url=%s",serverIP);
+
+    if(!findURL(mysql, list, query))
+    {
+        return 0;
+    }
+    sql_result = mysql_store_result(mysql);
+
+    memset(query, 0, sizeof(query));
+
+    if(sql_result == NULL)
+        printf("No Result.\n");
+    else
+    {
+        while((row = mysql_fetch_row(sql_result)))
+        {
+            if(strcmp(serverIP, row[1]))
+            {
+                sprintf(query,"delete from %s where url=%s",list, serverIP);
+                if(mysql_query(mysql, query))
+		{
+		    return 0;
+		}
+		return 1;
+            }
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char *args[])
