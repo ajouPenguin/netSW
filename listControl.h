0e94dcbaafdd73a9761b4766b0f55c83f2e7720e
root <root@ip-172-31-6-19.ap-northeast-2.compute.internal>
2017-06-03 04:14:03 +0000

    structure update

--- listControl.h
+++ listControl.h
@@ -3,9 +3,24 @@
 #include <iostream>
 #include <mysql.h>
 #include <memory.h>
+#include <string>
+
+using namespace std;
+
+#define CHARMAX 255
 
 using namespace std;
 
 int findURL(MYSQL *mysql, char* list, char* string);
 int insertURL(MYSQL *mysql, char* list, char* serverIP);
 int deleteURL(MYSQL *mysql, char* list, char* serverIP); 
+
+
+struct listItem
+{
+        int idx;
+        char url[CHARMAX];
+        unsigned int date;
+        char status[8];
+	string reason;
+};
