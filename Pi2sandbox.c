b10fdf1be8f6b273119422a4d70dddbd7b70e345
hhk2945 <hhk2945@naver.com>
2017-06-08 23:42:46 -0700

    Pi2sandbox

--- Pi2sandbox.c
+++ Pi2sandbox.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAXLEN 1024
 
@@ -17,9 +18,9 @@ void pi2sand(char url[], char cookie[]){
 	char *servIP; /* Server IP addr. (dotted quad) */
 	char buf[MAXLEN]; 
 	unsigned int bufLen;
-	int byteSend;
 	int bytesRcvd; /* Bytes read in single recv() */
-	int totalBytesRcvd; /* Total bytes read */
+	int status;
+	char* reason, checksum;
 	servIP = "192.168.200.119"; /* server IP address (dotted quad) */
 
 	echoServPort = 9700; /* port */
@@ -36,15 +37,45 @@ void pi2sand(char url[], char cookie[]){
 	//no bind()
 	memset(buf,0,MAXLEN);
 
-	sprintf(buf, "{\nrequest : {\n\turl : %s,\n\tcookie : %s\n\t}\n}", url, cookie);
-	
+	sprintf(buf, "{\nrequest : {\n\t\"url\" : %s,\n\t\"cookie\" : %s\n\t}\n}", url, cookie);
+
 	/* Send the string to the server */
 	bufLen = strlen(buf);
 	buf[bufLen] = '\0';
 
 
-	byteSend = sendto(sock, buf, bufLen, 0, (struct sockaddr*)&echoServAddr, sizeof(echoServAddr));
+	sendto(sock, buf, bufLen, 0, (struct sockaddr*)&echoServAddr, sizeof(echoServAddr));
+	fromSize = sizeof(fromAddr);
+	memset(buf,0,MAXLEN);
 
+	/* Receive the same string back from the server */
+	if(bytesRcvd = recvfrom (sock, buf, MAXLEN, 0,(struct sockaddr*)&fromAddr, &fromSize) < 0){
+		 fprintf(stderr, "recvfrom() error!\n");	
+	
+	buf[bytesRcvd] = '\0';
+	printf("Receive message: %s\n", buf);
+	
+	if(strstr(buf, "true") != NULL){
+		status = true;
+	}
+	else status = false;
+	
+	reason = strstr(buf, "reason") + 9;
+	checksum = strstr(buf, "checksum") + 11; 
+
+    	printf("\nreason : ");
+    	for(i = 0; reason[i]!= ','; i++)
+    	{
+    	 	printf("%c", reason[i]);
+    	}
+
+    	printf("\nchecksum : ");
+    	for(i = 0; checksum[i] != '}'; i++)
+    	{
+    	 	printf("%c", checksum[i]);
+    	}
+    	printf("\n");
+	
 	close (sock);
 	
 }
