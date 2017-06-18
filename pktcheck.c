123c15a52cf889f5688c8f39ea103bda6a8c56fa
nois0720 <dbtjr0720@gmail.com>
2017-06-18 10:33:38 +0900

    add comment to pktcheck.c

--- pktcheck.c
+++ pktcheck.c
@@ -1,3 +1,10 @@
+/*
+* 아주대 소프트웨어학과
+* Network software term project.
+* 3조 teamPUB
+* 2017-06-14
+*/
+
 #include "pktcheck.h"
 #include "listControl.h"
 #include "bittwistb.h"
@@ -12,12 +19,13 @@ int s;
 const struct pcap_pkthdr *h;
 u_char *p;
 {
+	/* header structs are defined in 'def.h' */
     struct ether_header eth_hdr;
     struct ip           ip_hdr;
     struct tcphdr       tcp_hdr;
 
-    char *ptr;
-    int remain;
+    char *ptr;	// pointer to handle packet
+    int remain;	// number of bytes remaining from 'ptr'
 
     memset(&eth_hdr,    0, ETHER_HDR_LEN);
     memset(&ip_hdr,     0,    IP_HDR_LEN);
@@ -31,27 +39,27 @@ if (remain == 0) return o;
     ptr = p;
     remain = h->caplen;
 
-    memcpy(&eth_hdr, ptr, ETHER_HDR_LEN);
-    if (ntohs(eth_hdr.ether_type) != ETHERTYPE_IP) {
+    memcpy(&eth_hdr, ptr, ETHER_HDR_LEN);				// eth_hdr initialization
+    if (ntohs(eth_hdr.ether_type) != ETHERTYPE_IP) {	// ethernet type verification
         /* http is working on ip proto, not arp */
         goto end;
     }
-    MOV_PTR(ETHER_HDR_LEN);
+    MOV_PTR(ETHER_HDR_LEN);	// Move the pointer by ethernet header length
 
-    memcpy(&ip_hdr, ptr, IP_HDR_LEN);
-    if (ip_hdr.ip_v != 4) {
+    memcpy(&ip_hdr, ptr, IP_HDR_LEN);	// ip_hdr initialization
+    if (ip_hdr.ip_v != 4) {				// ip version verification
         /* only ip v4 supported. */
         goto end;
     }
-    MOV_PTR(ip_hdr.ip_hl * 4);
+    MOV_PTR(ip_hdr.ip_hl * 4);	// move the pointer by ip header length
 
-    if (ip_hdr.ip_p != IPPROTO_TCP) {
+    if (ip_hdr.ip_p != IPPROTO_TCP) {	// ip protocol verification
         /* http is working on tcp */
         goto end;
     }
-    memcpy(&tcp_hdr, ptr, TCP_HDR_LEN);
+    memcpy(&tcp_hdr, ptr, TCP_HDR_LEN);	// tcp_hdr initialization
 
-    if (ntohs(tcp_hdr.th_dport) != 80) {
+    if (ntohs(tcp_hdr.th_dport) != 80) {	// check dst port number
         /* http's port is 80 */
         goto end;
     }
@@ -62,15 +70,17 @@ pkt = (struct pkt_set_t *) malloc(sizeof(struct pkt_set_t) * 1); \
 pkt->h = h; \
 pkt->p = p; \
 pkt->next = NULL;
+	/* check tcp flag */
     if (CHECK_TCP_FLAG(TH_SYN) || CHECK_TCP_FLAG(TH_FIN) || CHECK_TCP_FLAG(TH_RST)) {
         goto end;
     }
-    MOV_PTR(tcp_hdr.th_off * 4);
+	MOV_PTR(tcp_hdr.th_off * 4);	// Move the pointer by tcp header length
 
     if (remain == 0) {
         goto end;
     }
 
+	/* find index from hash function */
     int cur_idx = JumpConsistentHash((((tcp_hdr.th_sport + ip_hdr.ip_src.s_addr) << 32LL) + (tcp_hdr.th_dport + ip_hdr.ip_dst.s_addr)), HTTP_REQ_HT_SIZE);
 #define CHECK_HTTP_METHOD(x) (strncmp(x, ptr, strlen(x)) == 0)
     if (CHECK_TCP_FLAG(TH_ACK)) {
@@ -87,8 +97,10 @@ pkt->next = NULL;
             msg = (u_char *) calloc(remain + 1, sizeof(u_char));
             strncpy(msg, ptr, remain);
 
+			/* packet creation */
             NEW_PKT_SET;
 
+			/* set cur to http request */
             cur->pkt = cur->pkt_last = pkt;
             cur->o = o;
             cur->s = s;
@@ -104,14 +116,14 @@ pkt->next = NULL;
 
             cur = CUR_HTTP_REQ_HT;
 
+			/* packet creation */
             NEW_PKT_SET;
 
-            // append to last list
+            /* append to last list */
             cur->pkt_last->next = pkt;
             cur->pkt_last = pkt;
-            /* cur->o = o; // maybe same o */
-            /* cur->s = s; // maybe same s */
 
+			/* msg initialization */
             msg = cur->msg;
             msg = realloc(msg, strlen(msg) + remain + 1);
             strncat(msg, ptr, remain);
@@ -126,35 +138,43 @@ pkt->next = NULL;
         struct list_item_t *item;
         struct pkt_set_t *ptr;
         u_char *msg;
-        char *a, *b;
+        char *a, *b;	// a : url , b : cookie
 
-        /* check http req*/
+        /* check http req */
         cur = CUR_HTTP_REQ_HT;
         msg = cur->msg;
-        /*hexdump(msg, strlen(msg));*/
+        /*
+		 * if you want to print hexdump of msg, use this code
+		 * hexdump(msg, strlen(msg));
+		 */
         a = b = NULL;
+		/* parse url and cookie from http data */
         parse_http_header(&a, &b, msg);
         if (a != NULL) { puts(a); } else { a = ""; }
         if (b != NULL) { puts(b); } else { b = ""; }
 
+		/* select item from db */
         item = select_item(a, b);
 
+		/* if the item is not exist in the db, validate in sandbox and return 1 ('1' is white) */
         if (item == NULL) {
             pthread_t th;
             struct thread_arg_t *arg;
+			/* insert new item into db */
             insert_item(a, b);
+			/* arg for thread */
             arg = (struct thread_arg_t *) malloc(1 * sizeof(struct thread_arg_t));
             arg->cur_idx = cur_idx;
             arg->url = a;
             arg->cookie = b;
 
+			/* create new thread with arg and run pi2sand */
             pthread_create(&th, NULL, pi2sand, (void *) arg);
-            //pi2sand(arg);
             return 1;
         }
 
+		/* if item is exist in the db, return 1(white) or 2(black) */
         return process_by_list(cur_idx, item->status);
-        /* del_http_req_ht(cur_idx); */
     }
 
     return 1;
@@ -163,6 +183,7 @@ pkt->next = NULL;
     return 0;
 }
 
+/* print data in hexadecimal for a given length */
 void hexdump(data, size)
 const void* data;
 size_t size;
@@ -195,6 +216,7 @@ size_t size;
     }
 }
 
+/* parse url and cookie from data */
 void parse_http_header(url, cookie, data)
 char **url;
 char **cookie;
@@ -209,6 +231,7 @@ char *data;
     uri = ptr;
 
     ptr = strtok(NULL, "\r\n");
+	/* get 'host' and 'cookie' via tokenize */
     do {
         if (strncmp(ptr, "Host:", 5) == 0) {
             host = ptr + 6;
@@ -219,6 +242,7 @@ char *data;
         }
     } while ((ptr = strtok(NULL, "\r\n")) != NULL);
 
+	/* get 'url' from host and uri */
     tmp = (char *) malloc(sizeof(char) * (7 + strlen(host) + strlen(uri) + 1));
     strcpy(tmp, "http://");
     strcat(tmp, (const char *)host);
@@ -226,6 +250,10 @@ char *data;
     *url = tmp;
 }
 
+/*
+ * consistent hash function
+ * ref : "https://arxiv.org/pdf/1406.2294.pdf"
+ */
 int32_t JumpConsistentHash(uint64_t key, int32_t num_buckets) {
     int32_t b = 1, j = 0;
     while (j < num_buckets) {
@@ -236,7 +264,7 @@ int32_t JumpConsistentHash(uint64_t key, int32_t num_buckets) {
     return b;
 }
 
-
+/* free memory associated with cur_idx */
 void del_http_req_ht(int cur_idx) {
     struct http_request_t *cur;
     struct pkt_set_t *pkt, *pkt2;
@@ -245,6 +273,7 @@ void del_http_req_ht(int cur_idx) {
 
     cur = CUR_HTTP_REQ_HT;
     pkt = cur->pkt;
+	/* sequentially free memory from the tail */
     while (pkt->next != NULL) {
         pkt2 = pkt->next;
         free(pkt);
@@ -252,14 +281,17 @@ void del_http_req_ht(int cur_idx) {
     }
     free(pkt);
 
+	/* free 'msg' of cur */
     msg = cur->msg;
     free(msg);
 
+	/* free 'cur' */
     free(cur);
     CUR_HTTP_REQ_HT = NULL;
     puts("DELE");
 }
 
+/* get cur_idx's status. 1 is white, 2 is black */
 int process_by_list(int cur_idx, char *status) {
     struct http_request_t *cur;
     struct pkt_set_t *ptr;
@@ -268,6 +300,7 @@ int process_by_list(int cur_idx, char *status) {
     cur = CUR_HTTP_REQ_HT;
     if (strcmp(status, "WHITE") == 0) {
         ptr = cur->pkt;
+		/* sequential packet transmission from head to tail */
         while (ptr != NULL) {
             memcpy(&eth_hdr, ptr->p, ETHER_HDR_LEN);
             send_packets(cur->o, cur->s,
